0c9275398d3a873245ba9979b4aa17227f28b2df
Elias Friedman <eliasfriedman@Eliass-MBP.hsd1.mn.comcast.net>
2019-01-26 11:19:04 -0600

    new route at /polls/json

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+
 
 def index (request):
     return HttpResponse('<h1>Polls</h1>')
+
+def json (request):
+    return JsonResponse({'foo': 'bar'})
