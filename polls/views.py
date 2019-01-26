43a08c743fd09ba47acb67014b0ba9d1ef763dac
Elias Friedman <eliasfriedman@Eliass-MBP.hsd1.mn.comcast.net>
2019-01-26 09:11:45 -0600

    made view for /polls

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 
-def index(req):
-    return HttpResponse("Hello, I am a django server.")
+def index (request):
+    return HttpResponse('<h1>Polls</h1>')
